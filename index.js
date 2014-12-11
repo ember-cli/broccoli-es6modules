aac0dab3195a155eed6e6ad29ae0264683381ce1
Edward Faulkner <ef@alum.mit.edu>
2014-12-11 05:28:41 -0500

    newer dep

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var Transpile = require('./transpile');
 var SourceMapConcat = require('broccoli-sourcemap-concat');
+var Funnel = require('broccoli-funnel');
+var merge = require('broccoli-merge-trees');
 
 module.exports = function(inputTree, options) {
-  return new SourceMapConcat(new Transpile(inputTree, options), {
-    header: options.loaderFile,
+  var transpiled = new Transpile(inputTree, options);
+
+  var legacies = [];
+  if (options.loaderFile) {
+    legacies.push(options.loaderFile);
+  }
+  if (options.legacyFilesToAppend) {
+    legacies = legacies.concat(options.legacyFilesToAppend);
+  }
+
+  var legacyTree = new Funnel(inputTree, { files: legacies });
+  var both = merge([transpiled, legacyTree]);
+  return new SourceMapConcat(both, {
+    headerFiles: options.loaderFile ? [options.loaderFile] : null,
     inputFiles: options.inputFiles,
-    outputFile: options.outputFile
+    outputFile: options.outputFile,
+    footerFiles: options.legacyFilesToAppend
   });
 };
